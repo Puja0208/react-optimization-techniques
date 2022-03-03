6cc416ac32bd70000a2bc9fa76b4d87f2a231e38
puja.rani <puja.rani@ril.com>
2022-03-03 07:43:55 +0530

    feat: Use of useMemo to store sorted demolist

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { useState, useCallback } from "react";
-import Button from "./components/UI/Button/Button";
-import DemoOutput from "./components/Demo/DemoOutput";
+import React, { useState, useCallback, useMemo } from "react";
+
 import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
-  console.log("APP_RUNNING");
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
 
-  const clickHandler = useCallback(() => {
-    if (allowToggle) setShowParagraph((prevShow) => !prevShow);
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow toggling</Button>
-      <Button onClick={clickHandler}>Show button</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
